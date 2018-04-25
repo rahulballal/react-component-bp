2cc535dae29b78503957cf9ddd4fbbe9412e5c09
Rahul Ballal <rahul.ballal@shroogal.com.au>
2018-04-25 21:21:19 +1000

    chore: initial setup

---
+++ src/dev-entrypoint.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { hot } from 'react-hot-loader'
+import { Hello } from './entry'
+
+
+const AppShell = () => (<div>
+  <Hello />
+</div>)
+
+const HotReloadableApp = hot(module)(AppShell)
+
+ReactDOM.render(<HotReloadableApp/>, document.getElementById('app'))
